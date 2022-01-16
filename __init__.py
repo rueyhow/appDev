ced96552a436c9334b418ff910d54d45b8921f82
rueyhow <rahow32@gmail.com>
2022-01-17 05:09:06 +0800

    updated 17/1/2022

--- __init__.py
+++ __init__.py
@@ -11,9 +11,9 @@ from wtforms.validators import InputRequired , Email , Length
 from flask_bootstrap import Bootstrap as b
 import shelve
 from forms import CreateUserForm , ExistingMember
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy , inspect
 from sqlalchemy import Column , String , Integer
-from datetime import datetime, timedelta
+from datetime import date
 import uuid as uuid
 import sys
 from werkzeug.security import generate_password_hash , check_password_hash
@@ -92,15 +92,18 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(50))
     password = db.Column(db.String(80))
     gender = db.Column(db.String(6))
-    phone_number = db.Column(db.Integer() , unique = True)
+    phone_number = db.Column(db.Integer() , unique = False)
     profile_pic = db.Column(db.LargeBinary, nullable= True , default = ICON)
     base64 = db.Column(db.String(64) , nullable = True , default = base )
+    date = db.Column(db.String(10) , unique = False , nullable = False)
 
-# def save_image(profile_pic):
-#     f_text = os.path.splitext(profile_pic.filename)
-#     picture_fn = str(uuid.uuid1()) + str(f_text)
-#     picture_path = os.path.join(app.root_path + 'static/profilepics/' + str(picture_fn))
-#     return picture_fn
+
+# table = inspect(User)
+# for column in table.c:
+#     print(column.name)
+# row = User.query.get('username')
+# print(row)
+
 
 @login_manager.user_loader
 def loaded(id_user):
@@ -112,10 +115,15 @@ def Home_Page():
     if form2.validate_on_submit():
         hash = generate_password_hash(form2.password.data , method = 'sha256')
         user_ID = int(id(form2.username.data))
-        new_user = User(username = form2.username.data , email = form2.email.data , password = hash , gender = form2.gender.data , id = user_ID , phone_number = form2.phone_number.data)
-        db.session.add(new_user)
-        db.session.commit()
-        return '<h1> New user has been added! </h1>'
+        user_by_name = User.query.filter_by(username=form2.username.data).first()
+        if user_by_name:
+            return '<h1> Username taken </h1>'
+        else:
+            today = date.today()
+            new_user = User(username = form2.username.data , email = form2.email.data , password = hash , gender = form2.gender.data , id = user_ID , phone_number = form2.phone_number.data , date = today.strftime("%d/%m/%Y"))
+            db.session.add(new_user)
+            db.session.commit()
+            return '<h1> New user has been added! </h1>'
     forms = ExistingMember()
     if forms.validate_on_submit():
         user = User.query.filter_by(username = forms.username.data).first()
@@ -123,7 +131,8 @@ def Home_Page():
             if check_password_hash(user.password , forms.password.data):
                 login_user(user , remember = forms.remember.data)
                 return render_template('index.html' , form1 = form2 , form = forms)
-            return '<h1> Invalid username or password </h1>'
+            return '<h1> Invalid password </h1>'
+        return '<h1> Invalid username</h1>'
         # return '<h1>' + form1.username.data + " " + form1.password.data + "</h1>"
     return render_template('index.html' , form1 = form2 , form = forms)
 
@@ -141,10 +150,23 @@ def logout():
     flash('You have been logged out')
     return redirect('/')
 
+# @app.route('/delete')
+# def delete():
+#     user_to_delete = User.query.filter_by(username = current_user.username).first()
+#     form = CreateUserForm()
+#     users_table = User.query.all()
+#     try:
+#         db.session.delete(user_to_delete)
+#         db.session.commit()
+#         flash('User has been deleted')
+#     except:
+#         return redirect('/')
+
 @app.route('/dashboard/dist/dash.html' , methods = ['POST' , 'GET'])
 def dash():
     form = CreateUserForm()
     name_to_update = User.query.get(current_user.id)
+    users_table = User.query.all()
     if request.method == 'POST':
         # UPDATING USER INFORMATION
         name_to_update.username = request.form['username']
@@ -162,12 +184,13 @@ def dash():
         try:
             db.session.commit()
             flash('User updated successfully')
-            return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form , profile_pic = base64_message)
+            return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form , profile_pic = base64_message , users_table = users_table)
         except:
             flash('error')
-            return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form , profile_pic = base64_message)
+            return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form , profile_pic = base64_message , users_table = users_table)
     else:
-        return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form)
+        return render_template('dashboard/dist/dash.html' , name_to_update = name_to_update , form = form , users_table = users_table)
+    
 
 
 @app.route('/sp/shopping/dist/index2.html')
@@ -176,6 +199,7 @@ def shopping():
 @app.route('/index.html')
 def home():
     return render_template('index.html')
+    
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
     db.create_all()
