0f68464ff356b4eec28556bb05059cbd3a564028
thooyavan <govindarajuthooyavan@gmail.com>
2022-01-20 09:06:24 +0800

    hello

--- __init__.py
+++ __init__.py
@@ -27,6 +27,7 @@ from io import BytesIO
 import io
 import base64
 from wtforms.validators import ValidationError
+from datetime import datetime
 
 
 
@@ -245,7 +246,39 @@ def updateuser(id):
     if request.method == 'POST':
         return render_template('updateuser.html' , form = updateForm , user = ID)
     return render_template('updateuser.html' , form = updateForm , user = ID)
-    
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+db1 = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Feedback('{self.name}')"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     os.environ['FLASK_ENV'] = 'development'
     db.create_all()
