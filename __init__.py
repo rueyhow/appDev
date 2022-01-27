58bd03a3b2f71b4eec1a7d62fbc22b5621b36da4
Putra Ammar <putra.ammar470@gmail.com>
2022-01-27 08:28:14 +0800

    idk

--- __init__.py
+++ __init__.py
@@ -194,7 +194,7 @@ class Category(db.Model):
 def loaded(id_user):
     return User.query.get(int(id_user))
 
-@app.route('/', methods = ['GET' , 'POST'])
+@app.route('/index.html', methods = ['GET' , 'POST'])
 def Home_Page():
     form2 = CreateUserForm()
     if form2.validate_on_submit():
@@ -309,7 +309,11 @@ def shopping():
     return render_template('/sp/shopping/dist/index2.html')
 
 
-
+@app.route('/')
+def home1():
+    forms = ExistingMember()
+    form2 = CreateUserForm()
+    return render_template('index.html' , form1 = form2 , form = forms)
 
 
 @app.route('/updateuser/<int:id>' , methods = ['POST' , 'GET'])
@@ -329,6 +333,9 @@ def updateuser(id):
 
 
 
+@app.route('/feedback.html')
+def feedback():
+    return render_template('feedback.html')
 
 
 # end user login/register part
@@ -745,8 +752,6 @@ def clearcart():
         return redirect(url_for('home'))
     except Exception as e:
         print(e)
-
-
 
 
 
